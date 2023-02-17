3413cec774138982b25bebc79d519914e14bb737
Harry <harrywickham@live.co.uk>
2023-02-17 14:48:33 +0000

    add apis and edit layout

--- lib/api.ts
+++ lib/api.ts
@@ -1,49 +1,33 @@
-const API_URL = process.env.WORDPRESS_API_URL
+const API_URL = process.env.WORDPRESS_API_URL;
 
-async function fetchAPI(query = '', { variables }: Record<string, any> = {}) {
-  const headers = { 'Content-Type': 'application/json' }
+async function fetchAPI(query = "", { variables }: Record<string, any> = {}) {
+  const headers = { "Content-Type": "application/json" };
 
   if (process.env.WORDPRESS_AUTH_REFRESH_TOKEN) {
     headers[
-      'Authorization'
-    ] = `Bearer ${process.env.WORDPRESS_AUTH_REFRESH_TOKEN}`
+      "Authorization"
+    ] = `Bearer ${process.env.WORDPRESS_AUTH_REFRESH_TOKEN}`;
   }
 
   // WPGraphQL Plugin must be enabled
   const res = await fetch(API_URL, {
     headers,
-    method: 'POST',
+    method: "POST",
     body: JSON.stringify({
       query,
       variables,
     }),
-  })
+  });
 
-  const json = await res.json()
+  const json = await res.json();
   if (json.errors) {
-    console.error(json.errors)
-    throw new Error('Failed to fetch API')
+    console.error(json.errors);
+    throw new Error("Failed to fetch API");
   }
-  return json.data
+  return json.data;
 }
 
-export async function getPreviewPost(id, idType = 'DATABASE_ID') {
-  const data = await fetchAPI(
-    `
-    query PreviewPost($id: ID!, $idType: PostIdType!) {
-      post(id: $id, idType: $idType) {
-        databaseId
-        slug
-        status
-      }
-    }`,
-    {
-      variables: { id, idType },
-    }
-  )
-  return data.post
-}
-
+//Get all posts returning their slugs so we know if the page requested is a real post
 export async function getAllPostsWithSlug() {
   const data = await fetchAPI(`
     {
@@ -55,61 +39,67 @@ export async function getAllPostsWithSlug() {
         }
       }
     }
-  `)
-  return data?.posts
+  `);
+  return data?.posts;
 }
 
-export async function getAllPostsForHome(preview) {
+//Get the latest 20 posts to display on the homepage
+export async function getAllPostsForHome() {
   const data = await fetchAPI(
     `
-    query AllPosts {
-      posts(first: 20, where: { orderby: { field: DATE, order: DESC } }) {
+    fragment PostFields on Post {
+      title
+      excerpt
+      slug
+      date
+      featuredImage {
+        node {
+          sourceUrl
+        }
+      }
+      author {
+        node {
+          name
+          firstName
+          lastName
+          avatar {
+            url
+          }
+        }
+      }
+      categories {
         edges {
           node {
-            title
-            excerpt
-            slug
-            date
-            featuredImage {
-              node {
-                sourceUrl
-              }
-            }
-            author {
-              node {
-                name
-                firstName
-                lastName
-                avatar {
-                  url
-                }
-              }
-            }
+            name
+          }
+        }
+      }
+      tags {
+        edges {
+          node {
+            name
           }
         }
       }
     }
-  `,
-    {
-      variables: {
-        onlyEnabled: !preview,
-        preview,
-      },
+    query LatestPosts {
+      posts(first: 20, where: { orderby: { field: DATE, order: DESC }, tagNotIn: ["featured"] }) {
+        edges {
+          node {
+            ...PostFields
+          }
+        }
+      }
     }
-  )
+  `
+  );
 
-  return data?.posts
+  return data?.posts;
 }
 
-export async function getPostAndMorePosts(slug, preview, previewData) {
-  const postPreview = preview && previewData?.post
-  // The slug may be the id of an unpublished post
-  const isId = Number.isInteger(Number(slug))
-  const isSamePost = isId
-    ? Number(slug) === postPreview.id
-    : slug === postPreview.slug
-  const isDraft = isSamePost && postPreview?.status === 'draft'
-  const isRevision = isSamePost && postPreview?.status === 'publish'
+//Get the post based on the slug and then the latest 2 posts to show at the bottom of the post
+// This is used for the post page
+export async function getPostAndMorePosts(slug) {
   const data = await fetchAPI(
     `
     fragment AuthorFields on User {
@@ -119,6 +109,7 @@ export async function getPostAndMorePosts(slug, preview, previewData) {
       avatar {
         url
       }
+      description
     }
     fragment PostFields on Post {
       title
@@ -154,10 +145,6 @@ export async function getPostAndMorePosts(slug, preview, previewData) {
       post(id: $id, idType: $idType) {
         ...PostFields
         content
-        ${
-          // Only some of the fields of a revision are considered as there are some inconsistencies
-          isRevision
-            ? `
         revisions(first: 1, where: { orderby: { field: MODIFIED, order: DESC } }) {
           edges {
             node {
@@ -172,11 +159,74 @@ export async function getPostAndMorePosts(slug, preview, previewData) {
             }
           }
         }
-        `
-            : ''
-        }
       }
       posts(first: 3, where: { orderby: { field: DATE, order: DESC } }) {
+        edges {
+          node {
+            ...PostFields
+          }1
+        }
+      }
+    }
+  `,
+    {
+      variables: {
+        id: slug,
+        idType: "SLUG",
+      },
+    }
+  );
+
+  // Filter out the main post
+  data.posts.edges = data.posts.edges.filter(({ node }) => node.slug !== slug);
+  // If there are still 3 posts, remove the last one
+  if (data.posts.edges.length > 2) data.posts.edges.pop();
+
+  return data;
+}
+
+// Get the latest 3 posts based on their category
+// This is used for the category page
+export async function getPostsByCategory(category) {
+  const data = await fetchAPI(
+    `
+    fragment PostFields on Post {
+      title
+      excerpt
+      slug
+      date
+      featuredImage {
+        node {
+          sourceUrl
+        }
+      }
+      author {
+        node {
+          name
+          firstName
+          lastName
+          avatar {
+            url
+          }
+        }
+      }
+      categories {
+        edges {
+          node {
+            name
+          }
+        }
+      }
+      tags {
+        edges {
+          node {
+            name
+          }
+        }
+      }
+    }
+    query PostsByCategory($category: String!) {
+      posts(first: 3, where: { orderby: { field: DATE, order: DESC }, categoryName: $category }) {
         edges {
           node {
             ...PostFields
@@ -187,26 +237,85 @@ export async function getPostAndMorePosts(slug, preview, previewData) {
   `,
     {
       variables: {
-        id: isDraft ? postPreview.id : slug,
-        idType: isDraft ? 'DATABASE_ID' : 'SLUG',
+        category,
       },
     }
-  )
+  );
 
-  // Draft posts may not have an slug
-  if (isDraft) data.post.slug = postPreview.id
-  // Apply a revision (changes in a published post)
-  if (isRevision && data.post.revisions) {
-    const revision = data.post.revisions.edges[0]?.node
+  return data;
+}
 
-    if (revision) Object.assign(data.post, revision)
-    delete data.post.revisions
-  }
+// Get a list of the catagories that have posts
+// This is used for the category page
+export async function getCategories() {
+  const data = await fetchAPI(
+    `
+    query Categories {
+      categories {
+        edges {
+          node {
+            name
+          }
+        }
+      }
+    }
+  `
+  );
 
-  // Filter out the main post
-  data.posts.edges = data.posts.edges.filter(({ node }) => node.slug !== slug)
-  // If there are still 3 posts, remove the last one
-  if (data.posts.edges.length > 2) data.posts.edges.pop()
+  return data;
+}
 
-  return data
+// Get the latest featured post to display on the homepage
+// This is used for the homepage
+export async function getFeaturedPost() {
+  const data = await fetchAPI(
+    `
+    fragment PostFields on Post {
+      title
+      excerpt
+      slug
+      date
+      featuredImage {
+        node {
+          sourceUrl
+        }
+      }
+      author {
+        node {
+          name
+          firstName
+          lastName
+          avatar {
+            url
+          }
+        }
+      }
+      categories {
+        edges {
+          node {
+            name
+          }
+        }
+      }
+      tags {
+        edges {
+          node {
+            name
+          }
+        }
+      }
+    }
+    query FeaturedPost {
+      posts(first: 1, where: { orderby: { field: DATE, order: DESC }, tag: "featured" }) {
+        edges {
+          node {
+            ...PostFields
+          }
+        }
+      }
+    }
+  `
+  );
+
+  return data;
 }
